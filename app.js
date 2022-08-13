a98a48949c33db27456cab14e83445172392f906
Viviane de Lima <delimaviviane@yahoo.com.br>
2022-08-13 13:15:11 -0300

    first commit

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ app.use(express.json());
 const produtos =[{ nome:"Celular", preco: 1500, id:1 }]
 
 //CREATE
-app.post('/paes', (req, res) => {
+app.post('/produtos', (req, res) => {
     const { nome, preco, id } = req.body;
     produtos.push({ nome, preco, id});
     res.json(produtos);
 });
 
 //READ
-app.get('/paes', (req,res) =>{
+app.get('/produtos', (req,res) =>{
     res.json(produtos);
 });
 
 //UPDATE
-app.put("/paes/:index", (req, res) => {
+app.put("/produtos/:index", (req, res) => {
     const { index } = req.params;
     const { nome, preco, id } = req.body;
     produtos[index] = ({ nome, preco, id});
@@ -26,7 +26,7 @@ app.put("/paes/:index", (req, res) => {
 });
 
 //DELETE
-app.delete('/paes/:index', (req, res) => {
+app.delete('/produtos/:index', (req, res) => {
     const { index } = req.params;
     produtos.splice(index, 1);
     
